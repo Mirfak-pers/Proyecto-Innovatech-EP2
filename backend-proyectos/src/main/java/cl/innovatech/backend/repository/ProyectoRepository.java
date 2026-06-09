031bec9b5d2f8e2018754e3254603b7368a6c991
AlanGitUA <alangutierrez617@gmail.com>
2026-06-09 17:06:20 -0400

    fix: eliminar clase main duplicada

--- backend-proyectos/src/main/java/cl/innovatech/backend/repository/ProyectoRepository.java
+++
@@ -1,7 +0,0 @@
-package cl.innovatech.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cl.innovatech.backend.model.Proyecto;
-
-public interface ProyectoRepository extends JpaRepository<Proyecto, Long> {}
