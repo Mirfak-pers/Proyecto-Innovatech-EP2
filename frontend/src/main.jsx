bb2e57063a62c84996444173eb1e2b7c8dcba0b0
AlanGitUA <alangutierrez617@gmail.com>
2026-06-09 16:37:09 -0400

    cambios frontend y backend
    cambios al frontend y backend completo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./styles/app.css";
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRoutes from "./Routes/AppRoutes.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AppRoutes />
+  </React.StrictMode>
+);
